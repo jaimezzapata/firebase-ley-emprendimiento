f2192a4a6f1d3395518905cc4417b83e6aaf47e5
jaimezzapata <zapataval2304@gmail.com>
2023-10-04 07:52:05 -0500

    Se crear método de listado de usuarios

--- src/components/layouts/usuarios/ListadoUsuarios.jsx
+++ src/components/layouts/usuarios/ListadoUsuarios.jsx
@@ -1,6 +1,20 @@
 import Header from "../../helpers/Header";
+import { useState } from "react";
+import { dataBase } from "../../config/firebaseConfig";
+import { collection, getDocs } from "firebase/firestore";
 
 const ListadoUsuarios = () => {
+  const [usuarios, setUsuarios] = useState()
+
+  async function listarUsuarios(){
+   let usuariosCollection = collection(dataBase, 'usuarios')
+   let data = await getDocs(usuariosCollection)
+   console.log(usuariosCollection)
+   console.log(data)
+  }
+
+  listarUsuarios()
+
   return (
     <section>
       <Header />
